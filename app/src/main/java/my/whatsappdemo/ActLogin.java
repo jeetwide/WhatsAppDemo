27ed4a8a4ee72ed55f1cf2eb88f13adfae64a6de
Jeetendra Achtani <wide.angle456@gmail.com>
2019-05-02 13:01:12 +0530

    initial commit

--- app/src/main/java/my/whatsappdemo/ActLogin.java
+++ app/src/main/java/my/whatsappdemo/ActLogin.java
@@ -3,12 +3,19 @@ package my.whatsappdemo;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseUser;
 
 public class ActLogin extends AppCompatActivity {
 
     private FirebaseUser currentUser;
+    private Button login_button, phone_login_button;
+    private EditText user_email, user_pass;
+    private TextView need_new_account, forget_pass_link;
 
 
     @Override
@@ -16,18 +23,41 @@ public class ActLogin extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_act_login);
 
+        InitializeFields();
+
+        need_new_account.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SendUserToRegisterActivity();
+            }
+        });
 
     }
+
+    private void InitializeFields() {
+        login_button = findViewById(R.id.login_button);
+        phone_login_button = findViewById(R.id.phone_button);
+        user_email = findViewById(R.id.login_email);
+        user_pass = findViewById(R.id.login_password);
+        need_new_account = findViewById(R.id.need_new_account);
+        forget_pass_link = findViewById(R.id.forget_password_link);
+
+    }
+
     @Override
     protected void onStart() {
         super.onStart();
-        if(currentUser != null){
+        if (currentUser != null) {
             SendUserToMainActivity();
         }
 
     }
 
     private void SendUserToMainActivity() {
-        startActivity(new Intent(ActLogin.this,MainActivity.class));
+        startActivity(new Intent(ActLogin.this, MainActivity.class));
+    }
+
+    private void SendUserToRegisterActivity() {
+        startActivity(new Intent(ActLogin.this, ActRegister.class));
     }
 }
--- app/src/main/java/my/whatsappdemo/ActRegister.java
+++ app/src/main/java/my/whatsappdemo/ActRegister.java
@@ -1,13 +1,46 @@
 package my.whatsappdemo;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class ActRegister extends AppCompatActivity {
+
+    private Button create_account_btn;
+    private EditText user_email, user_pass;
+    private TextView already_have_account;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_act_register);
+
+        InitializeFields();
+
+        already_have_account.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SendUserToLoginActivity();
+            }
+        });
+    }
+
+
+    private void InitializeFields() {
+        create_account_btn = findViewById(R.id.register_button);
+
+        user_pass = findViewById(R.id.register_password);
+        user_email = findViewById(R.id.register_email);
+        already_have_account = findViewById(R.id.already_have_account);
+
+
+    }
+
+    private void SendUserToLoginActivity() {
+        startActivity(new Intent(ActRegister.this, ActLogin.class));
     }
 }
