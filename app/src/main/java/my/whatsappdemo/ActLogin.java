b106ca75b09fd3d3704d431ef31c41885ad18bd7
Jeetendra Achtani <wide.angle456@gmail.com>
2019-05-02 14:22:12 +0530

    initial commit

--- app/src/main/java/my/whatsappdemo/ActLogin.java
+++ app/src/main/java/my/whatsappdemo/ActLogin.java
@@ -1,13 +1,20 @@
 package my.whatsappdemo;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 public class ActLogin extends AppCompatActivity {
@@ -16,12 +23,17 @@ public class ActLogin extends AppCompatActivity {
     private Button login_button, phone_login_button;
     private EditText user_email, user_pass;
     private TextView need_new_account, forget_pass_link;
+    private FirebaseAuth mAuth;
+
+
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_act_login);
+
+        mAuth=FirebaseAuth.getInstance();
 
         InitializeFields();
 
@@ -32,6 +44,43 @@ public class ActLogin extends AppCompatActivity {
             }
         });
 
+        login_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AllowUserToLogin();
+            }
+        });
+
+    }
+
+    private void AllowUserToLogin() {
+
+        String email= user_email.getText().toString();
+        String password= user_pass.getText().toString();
+
+        if(TextUtils.isEmpty(email)){
+            Toast.makeText(this, "Please Enter the Email", Toast.LENGTH_SHORT).show();
+        }
+        if(TextUtils.isEmpty(password)){
+            Toast.makeText(this, "Please Enter the Password", Toast.LENGTH_SHORT).show();
+        }
+        else{
+
+            mAuth.signInWithEmailAndPassword(email,password)
+                    .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                                if(task.isSuccessful()){
+                                    SendUserToMainActivity();
+                                    Toast.makeText(ActLogin.this, "Logged in Successfully!!!", Toast.LENGTH_SHORT).show();
+                                }else{
+                                    String msg= task.getException().toString();
+                                    Toast.makeText(ActLogin.this, "Error :"+msg, Toast.LENGTH_SHORT).show();
+
+                                }
+                        }
+                    });
+        }
     }
 
     private void InitializeFields() {
--- app/src/main/java/my/whatsappdemo/ActRegister.java
+++ app/src/main/java/my/whatsappdemo/ActRegister.java
@@ -1,24 +1,36 @@
 package my.whatsappdemo;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class ActRegister extends AppCompatActivity {
 
     private Button create_account_btn;
     private EditText user_email, user_pass;
     private TextView already_have_account;
+    private FirebaseAuth mAuth;
+    ProgressDialog loadingBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_act_register);
 
+        mAuth=FirebaseAuth.getInstance();
         InitializeFields();
 
         already_have_account.setOnClickListener(new View.OnClickListener() {
@@ -27,6 +39,47 @@ public class ActRegister extends AppCompatActivity {
                 SendUserToLoginActivity();
             }
         });
+        create_account_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                CreateNewAccount();
+            }
+        });
+
+    }
+
+    private void CreateNewAccount() {
+        String email= user_email.getText().toString();
+        String password= user_pass.getText().toString();
+
+        if(TextUtils.isEmpty(email)){
+            Toast.makeText(this, "Please Enter the Email", Toast.LENGTH_SHORT).show();
+        }
+        if(TextUtils.isEmpty(password)){
+            Toast.makeText(this, "Please Enter the Password", Toast.LENGTH_SHORT).show();
+        }
+        else{
+            loadingBar.setTitle("Creating new Account");
+            loadingBar.setMessage("Please Wait while we are creating an account for you");
+            loadingBar.setCanceledOnTouchOutside(true);
+            loadingBar.show();
+
+            mAuth.createUserWithEmailAndPassword(email,password)
+            .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if(task.isSuccessful()){
+                        SendUserToLoginActivity();
+                        Toast.makeText(ActRegister.this, "Account Created", Toast.LENGTH_SHORT).show();
+                        loadingBar.dismiss();
+                    }else{
+                        String msg= task.getException().toString();
+                        Toast.makeText(ActRegister.this, "Error :"+msg, Toast.LENGTH_SHORT).show();
+                        loadingBar.dismiss();
+                    }
+                }
+            });
+        }
     }
 
 
@@ -36,6 +89,7 @@ public class ActRegister extends AppCompatActivity {
         user_pass = findViewById(R.id.register_password);
         user_email = findViewById(R.id.register_email);
         already_have_account = findViewById(R.id.already_have_account);
+        loadingBar=new ProgressDialog(this);
 
 
     }
